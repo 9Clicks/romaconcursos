7ab0561a285b843089b161d8117ec2d35bef66ae
Alexandro F. S. Matins <noveclicks@iMac-de-Alexandro.local>
2018-09-17 13:01:14 -0300

    Configuração de Layout e Bootstrap Instalado

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,18 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass([
-        '../../../resources/assets/sass/style.sass'
+        '../../../resources/assets/sass/style.sass',
+
     ], 'public/dist/css/layout.css');
+
+    mix.version('public/dist/css/layout.css');
+
+    mix.style([
+        '../../../node_modules/bootstrap/dist/css/bootstrap.css',
+        '../../../node_modules/bootstrap/dist/css/bootstrap-grid.css',
+        '../../../node_modules/bootstrap/dist/css/bootstrap-reboot.css',
+
+    ], 'public/dist/css/bootstrap.css');
+
+    mix.version('public/dist/css/bootstrap.css');
 });
