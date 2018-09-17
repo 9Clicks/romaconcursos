1a3537d118a766269e4af54ef7ad1734e65477c6
Alexandro F. S. Matins <noveclicks@iMac-de-Alexandro.local>
2018-09-17 13:10:04 -0300

    Scripts Bootstrap/Poppers/Bundle JS Configurados

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,26 @@ elixir(function(mix) {
     mix.style([
         '../../../node_modules/bootstrap/dist/css/bootstrap.css',
         '../../../node_modules/bootstrap/dist/css/bootstrap-grid.css',
-        '../../../node_modules/bootstrap/dist/css/bootstrap-reboot.css',
+        '../../../node_modules/bootstrap/dist/css/bootstrap-reboot.css'
 
     ], 'public/dist/css/bootstrap.css');
 
     mix.version('public/dist/css/bootstrap.css');
+
+    mix.scripts([
+        '../../../node_modules/bootstrap/dist/js/bootstrap.bunble.js',
+        '../../../node_modules/bootstrap/dist/js/bootstrap.js'
+
+    ], 'public/dist/js/bootstrap.js');
+
+    mix.scripts([
+        '../../../node_modules/popper.js/dist/popper.js',
+        '../../../node_modules/popper.js/dist/popper-utils.js'
+
+    ], 'public/dist/js/popper.js' );
+
+    mix.scripts([
+        '../../../node_modules/jquery/jquery.js'
+
+    ], 'public/dist/js/jQuery.js');
 });
