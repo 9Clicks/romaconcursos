10489d5be39845cb167b523e2707c49da594f9b9
Alexandro F. S. Matins <noveclicks@iMac-de-Alexandro.local>
2018-09-18 09:55:13 -0300

    Framework Bootstrap modificado para versão 3.3.7

--- gulpfile.js
+++ gulpfile.js
@@ -8,23 +8,14 @@ elixir(function(mix) {
     mix.version('public/dist/css/layout.css');
 
     mix.styles([
-        '../../../node_modules/bootstrap/dist/css/bootstrap.css',
-        '../../../node_modules/bootstrap/dist/css/bootstrap-grid.css',
-        '../../../node_modules/bootstrap/dist/css/bootstrap-reboot.css'
+        '../../../node_modules/bootstrap/dist/css/bootstrap.css'
 
     ], 'public/dist/css/bootstrap.css');
 
     mix.scripts([
-        '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js',
         '../../../node_modules/bootstrap/dist/js/bootstrap.js'
 
     ], 'public/dist/js/bootstrap.js');
-
-    mix.scripts([
-        '../../../node_modules/popper.js/dist/popper.js',
-        '../../../node_modules/popper.js/dist/popper-utils.js'
-
-    ], 'public/dist/js/popper.js' );
 
     mix.scripts([
         '../../../node_modules/jquery/jquery.js'
--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -5,8 +5,8 @@
 <head>
 
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> - Ipatinga/MG</title>
     <link rel="icon" type="image/png" href="{{ asset('assets/images/layout/favicon.png') }}" />
 
@@ -56,6 +56,5 @@
 </body>
 
     <script type="text/javascript" src="{{ asset('dist/js/bootstrap.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('dist/js/popper.js') }}"></script>
 
 </html>
