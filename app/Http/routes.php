4968c3e83860986c63d48b30cab412d41229b4c8
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-20 09:24:59 -0300

    Controller para Curso Bombeiro Militar Configurado

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,10 +9,13 @@ Route::get('/contatos', 'ContatoController@index');
 Route::get('/preparatorio/agente-penitenciario', '_preAgenteController@index');
 Route::get('/preparatorio/agente-penitenciario/detalhes/{id}', '_preAgenteController@detalhes');
 
+Route::get('/preparatorio/bombeiro-militar', '_preBombeiroMController@index');
+Route::get('/preparatorio/bombeiro-militar/detalhes/{id}', '_preBombeiroMController@detalhes');
 
 
 
-Route::get('/preparatorio/bombeiro-militar', 'PreBombeiroController@index');
+
+
 Route::get('/preparatorio/policia-civil', 'PrePoliciaCController@index');
 Route::get('/preparatorio/inss', 'PreInssController@index');
 Route::get('/preparatorio/prf', 'PrePRFController@index');
@@ -20,7 +23,7 @@ Route::get('/preparatorio/oab', 'PreOABController@index');
 
 Route::get('/preparatorio/enem', 'PreENEMController@index');
 
-Route::get('/preparatorio/bombeiro-militar/detalhes/{id}', 'PreBombeiroController@detalhes');
+
 Route::get('/preparatorio/policia-civil/detalhes/{id}', 'PrePoliciaCController@detalhes');
 
 Route::get('/preparatorio/enem/detalhes/{id}', 'PreENEMController@detalhes');
