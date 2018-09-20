30a6e85e9400ca1059927970f70ff0cea3bd2900
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-20 15:11:33 -0300

    Icone do titulo da página contato alterado.

--- resources/views/contato/index.blade.php
+++ resources/views/contato/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="col-xl-12 mt-4" id="tpl_title_pg">
-        <h1><span class="icon-addressbook1"></span> Contatos:</h1>
+        <h1><span class="icon-envelope1"></span> Contatos:</h1>
     </div>
 
 @stop
