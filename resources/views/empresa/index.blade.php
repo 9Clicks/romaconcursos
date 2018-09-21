49b7510d506a083c91126c415258100a11b8ac57
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-21 13:37:57 -0300

    Include Add. a Fotos da Galeria.

--- resources/views/empresa/index.blade.php
+++ resources/views/empresa/index.blade.php
@@ -31,66 +31,7 @@
     </div>
     <div class="col-xl-12">
         <div class="row mb-5" id="img-galeria">
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma01.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma01.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma02.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma02.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma03.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma03.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma04.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma04.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma05.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma05.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma06.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma06.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma07.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma07.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma08.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma08.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma09.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma09.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma10.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma10.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma11.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma11.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
-            <div class="col-xl-3 mt-4">
-                <a href="{{ asset('images/fotos/img_roma12.jpg') }}" data-lightbox="example-1">
-                    <img class="img-fluid" src="{{ asset('images/fotos/img_roma12.jpg') }}"  alt="image-1" />
-                </a>
-            </div>
+            @include('require.inc_galeria')
         </div>
     </div>
 
